c26bd4b84179ab209e697052526142d64a2a6cad
Ferdi Ramdhon <ferdirn@gmail.com>
2015-05-04 11:45:31 +0700

    update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/user/')
+@app.route('/user/<username>/')
+def user(username=None):
+    context = {'name': username, 'job': 'Hacker'}
+    return render_template('hello.html', name=username, job='Programmer')
 
 if __name__ == '__main__':
     app.run()
